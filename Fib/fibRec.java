21f92e39f2732477697f71e3ef3ba966b3507bac
Duberticus <166752792+Duberticus@users.noreply.github.com>
2024-12-25 14:09:18 -0600

    fibRec worked on

--- Fib/fibRec.java
+++ Fib/fibRec.java
@@ -1,18 +1,21 @@
 import java.util.Scanner;
 public class fibRec {
 
-    public int fibMethod(int fib){
-
-        
-        return 0;
+    public int fibMethod(int counter,int fib){
+        if(counter = 0){
+    return fib;
+        }else{
+            return fibMethod(counter-1 ,  fib);
+        }
     }
-
 
     public static void main(String[] args) {
         System.out.println("enter fib number");
         Scanner scan = new Scanner(System.in);
-        int fib = scan.nextInt();
-
+        int counter = scan.nextInt();
+        int base = 1;
+        int result = fibMethod();
+        System.out.println(result);
       
     }
 }
