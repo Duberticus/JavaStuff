090ebd508ce536eb3b99a7cb1e05074315b39ba1
Duberticus <166752792+Duberticus@users.noreply.github.com>
2024-12-30 01:34:29 -0600

    added Recursive fibonacci

--- Fib/fibRec.java
+++ Fib/fibRec.java
@@ -1,11 +1,15 @@
 import java.util.Scanner;
 public class fibRec {
 
-    public int fibMethod(int counter,int fib){
-        if(counter = 0){
-    return fib;
+    public int fibMethod(int counter){
+        System.out.println(counter);
+        if(counter == 0){
+            return 0;
+        }else if(counter == 1){
+            return 1;
         }else{
-            return fibMethod(counter-1 ,  fib);
+          //  System.out.println(counter);
+            return fibMethod(counter -1) + fibMethod(counter -2);
         }
     }
 
@@ -13,9 +17,9 @@ public class fibRec {
         System.out.println("enter fib number");
         Scanner scan = new Scanner(System.in);
         int counter = scan.nextInt();
-        int base = 1;
-        int result = fibMethod();
+        fibRec fib= new fibRec();
+        int result = fib.fibMethod(counter);
         System.out.println(result);
-      
+        scan.close();
     }
 }
